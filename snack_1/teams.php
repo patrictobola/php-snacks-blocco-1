c69224c48e8e6ce407a8505094c53ea9efa75670
patrictobola <tobola92@gmail.com>
2023-07-17 13:18:07 +0200

    done matches in php

---
+++ snack_1/teams.php
@@ -0,0 +1,29 @@
+<?php
+$matches = [
+    'match_1' => [
+        'home_team' => ['team' => 'Philadelphia 76ers', 'score' => 119],
+        'away_team' => ['team' => 'Boston Celtics', 'score' => 115]
+    ],
+    'match_2' => [
+        'home_team' => ['team' => 'Phoenix Suns', 'score' => 87],
+        'away_team' => ['team' => 'Denver Nuggets', 'score' => 97]
+    ],
+    'match_3' => [
+        'home_team' => ['team' => 'Miami Heat', 'score' => 105],
+        'away_team' => ['team' => 'New York Knicks', 'score' => 97]
+    ],
+    'match_4' => [
+        'home_team' => ['team' => 'Los Angeles Lakers', 'score' => 113],
+        'away_team' => ['team' => 'Golden State Warriors', 'score' => 121]
+    ],
+    'match_5' => [
+        'home_team' => ['team' => 'Miami Heat', 'score' => 111],
+        'away_team' => ['team' => 'Boston Celtics', 'score' => 105]
+    ],
+    'match_6' => [
+        'home_team' => ['team' => 'Sacramento Kings', 'score' => 108],
+        'away_team' => ['team' => 'Washington Wizards', 'score' => 118]
+    ],
+
+];
+var_dump($matches);
