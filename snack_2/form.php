1e54cfdb8c3bfaee31ee45bf8c2ff70370b0c4f6
patrictobola <tobola92@gmail.com>
2023-07-17 15:42:39 +0200

    validato ogni input del form

--- snack_2/form.php
+++ snack_2/form.php
@@ -16,9 +16,14 @@ $result = 'Accesso negato';
 </head>
 
 <body>
+    <!-- Il nome deve avere più di 3 lettere  -->
     <?php if (strlen($name) > 3) : ?>
-        <?php if (str_contains($email, '@') && str_contains($email, '.')) : $result = 'Accesso Consentito' ?>
+        <!-- La mail deve contenere la chiocciola e il punto -->
+        <?php if (str_contains($email, '@') && str_contains($email, '.')) : ?>
+            <!-- L'età dev'essere un numero. Se risulta tutto valido, solo a questo punto l'accesso è consentito  -->
+            <?php if (is_numeric($age)) : $result = 'Accesso Consentito' ?>
 
+            <?php endif ?>
         <?php endif ?>
     <?php endif ?>
     <h1><?= $result ?></h1>
